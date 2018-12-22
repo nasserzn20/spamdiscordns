4c6dc8e922f3236a9e3b9fdd3fa8e54adf837596
nasserzn20 <46067293+nasserzn20@users.noreply.github.com>
2018-12-22 11:56:13 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523446532221960204")
+setInterval(function() {
+channel.send(`nasserxdspam`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
